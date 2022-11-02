c9fb63f563e325dcfbe0ca339ecaf2861423281f
Leonel Hernandez Aguirre <44650454+LAguirr@users.noreply.github.com>
2022-11-02 23:43:00 +0000

    authentication by apikey added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect,flash, url_for
 from werkzeug.exceptions import abort
 import requests
 import json
@@ -8,17 +8,23 @@ app = Flask(__name__)
 
 url = "https://api.meraki.com/api/v1/"
 
-
 headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'X-Cisco-Meraki-API-Key': '',
+}
+
+def getOrganizations(apikey):
+  
+      headers = {
   'Content-Type': 'application/json',
   'Accept': 'application/json',
-  'X-Cisco-Meraki-API-Key': ''
-}
-def getOrganizations(): 
+  'X-Cisco-Meraki-API-Key': apikey,
+    }
       response = requests.request('GET', url + '/organizations', headers=headers)
       return response.json()
 
-def storesbyorganization(organization):
+def networksbyorganization(organization):
       response = requests.request("GET", url + 'organizations/'+organization+'/networks', headers=headers)
       return response.json()
       
@@ -33,11 +39,28 @@ def puertos(serial):
   puertos = response.json()
   return puertos
 
-  
-@app.route('/')
+
+@app.route('/',methods=["GET", "POST"])
 def index():
-    organizations = getOrganizations()
-    return render_template('index.html', posts=organizations)
+    if request.method == 'POST':
+      apikey = request.form['apikey']
+      if apikey:
+        headers['X-Cisco-Meraki-API-Key'] = apikey
+
+        return redirect(url_for('organizations',apikey = apikey))
+    return render_template('index.html')
+
+@app.route('/organizations/<apikey>')
+def organizations(apikey): 
+
+  organizations = getOrganizations(apikey)
+  return render_template('organizations.html',posts=organizations)
+
+@app.route('/networks/<id_organization>')
+def networks(id_organization):
+
+  networks = networksbyorganization(id_organization)
+  return render_template('networks.html', posts=networks)
 
 
 app.run(host='0.0.0.0', port=81)
